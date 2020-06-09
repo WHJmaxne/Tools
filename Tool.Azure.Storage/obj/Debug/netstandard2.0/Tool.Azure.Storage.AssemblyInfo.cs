cd593b9c882e23b0acdd12a8c18590a5d3118d6a
红杰 王 <whjmaxne@outlook.com>
2020-06-09 14:54:21 +0800

    none

--- Tool.Azure.Storage/obj/Debug/netstandard2.0/Tool.Azure.Storage.AssemblyInfo.cs
+++ Tool.Azure.Storage/obj/Debug/netstandard2.0/Tool.Azure.Storage.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("whj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tool.Azure.Storage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tool.Azure.Storage")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.2.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
